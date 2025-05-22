ef7960abc13851a396eddbe182347ed4632e63e2
Jose Antonio Campos Lora <jacamlor@gmail.com>
2025-05-22 12:31:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,18 @@ st.set_page_config(page_title="ALBATRON", layout="wide")
 
 class ReportPDF(FPDF):
     def header(self):
-        self.image(LOGO_PATH, x=10, y=8, w=30)
-        self.set_font("Arial", "B", 14)
-        self.set_xy(50, 10)
-        self.cell(0, 10, f"Nº Albarán: {self.albaran_number}", ln=True, align="L")
-        self.ln(10)
+    try:
+        self.image(LOGO_PATH, x=10, y=8, w=50)  # Aumentamos visibilidad
+    except RuntimeError:
+        self.set_font("Arial", "B", 12)
+        self.set_text_color(255, 0, 0)
+        self.cell(0, 10, "Logo no encontrado", ln=True)
+
+    self.set_font("Arial", "B", 14)
+    self.set_xy(70, 10)
+    self.cell(0, 10, f"Nº Albarán: {self.albaran_number}", ln=True, align="L")
+    self.ln(10)
+
 
     def chapter_subtitle(self, color, total_unidades):
         self.set_font("Arial", "B", 11)
